23eb2b22399d8b74f5ee31105a5e351cce5182d5
DaviZzZS2 <122384585+DaviZzZS2@users.noreply.github.com>
2023-10-05 22:16:42 -0300

    Apagando Funções que não irão servir para o funcionamento do Jogo

--- campoMinadoFuncional.js
+++ campoMinadoFuncional.js
@@ -23,19 +23,5 @@ function gerarTabuleiro(linhas, colunas) { //A função gerarTabuleiro recebe do
     }
     
     gerarTabuleiro(9,9)
- //Função que dá valor para os itens da matriz gerada anteriormente,usando a reutilização de funções;
- //A const matriz recebe a função geramatriz com seus parâmetros,logo depois a const matrizComNumerosAleatórios pega o resultado da função geraMatriz,que retorna uma lista,com listas dentro,todas preenchidas com 0 EX:[[0,0,0,0,0]],mapeia esse resultado da função,chegando na lista Interior,que é [0,0,0,0,0],só que apenas com um map,nao conseguiria mudar os valores de dentro dessa lista,então usando um map na lista Interior,gerada pelo primeiro map,consegue-se adentrar os itens da lista interior,assim dando uma função para que cada item da lista interior retorne um valor randômico e aproximado para baixo,que está entre 1 e 10;
- function valorParaAsMatrizes(linhas,colunas){
-    const matriz = geraMatriz(linhas,colunas);
-    const matrizComNumerosAleatorios = matriz.map((listaInterior) => listaInterior.map((itensDaListaInterior) => Math.floor(Math.random()*10)+1));
-    return matrizComNumerosAleatorios;
- }
- function filtraBombas(linhas,colunas){
-    const matrizComNumerosAleatorios = valorParaAsMatrizes(linhas,colunas);
-    const bomba = Math.floor(Math.random()*10);
-    console.log(bomba)
-    const bombasFiltradas = matrizComNumerosAleatorios.map((listaInterior) => listaInterior.filter((numeroDoArray) => numeroDoArray == bomba));
-    return bombasFiltradas
+
  
- }
- console.log(filtraBombas(9,9))
